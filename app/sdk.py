2416bbe440a21a967daff7999473489f3c488845
_ <_@coinjinja.com>
2018-10-15 16:09:56 +0900

    add api documents

--- app/sdk.py
+++ app/sdk.py
@@ -101,9 +101,26 @@ class CoinViewPay:
             if body[x] is None:
                 del body[x]
         r = http_session.post(self.BASE_URL + '/wallet', json=body)
+        print(r)
+        print(r.content)
         return r.json()
 
     def transfer(self, receiver_id: str, amount: str, asset_id: str, trace_id: str, memo: Optional[str]):
+        """
+            make a direct transfer
+
+            response
+                {
+                  "type": "transfer",
+                  "snapshot_id": "ab56be4c-5b20-41c6-a9c3-244f9a433f35",
+                  "opponent_id": "a465ffdb-4441-4cb9-8b45-00cf79dfbc46",
+                  "asset_id": "43d61dcd-e413-450d-80b8-101d5e903357",
+                  "amount": "-10",
+                  "trace_id": "7c67e8e8-b142-488b-80a3-61d4d29c90bf",
+                  "memo": "hello",
+                  "created_at": "2018-05-03T10:08:34.859542588Z"
+                }
+        """
         return self._request('POST', '/transfers', {
             'asset_id': asset_id,
             'opponent_id': receiver_id,
@@ -114,6 +131,37 @@ class CoinViewPay:
         }).get('data')
 
     def verify_payment(self, receiver_id: str, amount: str, asset_id: str, trace_id: str):
+        """
+            verify payment status
+
+            response
+                {
+                  "recipient": {
+                    "type": "user",
+                    "user_id": "a465ffdb-4441-4cb9-8b45-00cf79dfbc46",
+                    "identity_number": "20018",
+                    "full_name": "OS105",
+                    "avatar_url": "",
+                    "relationship": "",
+                    "mute_until": "0001-01-01T00:00:00Z",
+                    "created_at": "2018-04-25T05:37:10.06433488Z",
+                    "is_verified": false
+                  },
+                  "asset": {
+                    "type": "asset",
+                    "asset_id": "43d61dcd-e413-450d-80b8-101d5e903357",
+                    "chain_id": "43d61dcd-e413-450d-80b8-101d5e903357",
+                    "symbol": "EOS",
+                    "name": "EOS",
+                    "balance": "0",
+                    "public_key": "",
+                    "price_btc": "0.0776679",
+                    "price_usd": "715.394"
+                  },
+                  "amount": "10",
+                  "status": "pending"
+                }
+        """
         return self._request('POST', '/payments', {
             'asset_id': asset_id,
             'opponent_id': receiver_id,
@@ -122,23 +170,83 @@ class CoinViewPay:
         }).get('data')
 
     def list_assets(self):
+        """
+            list all your assets
+
+            response:
+                [
+                  {
+                    "asset_id": "07065d64-fd33-39b5-b275-9a2cc4806ef4",
+                    "balance": "999607",
+                    "chain_id": "43d61dcd-e413-450d-80b8-101d5e903357",
+                    "confirmations": 100,
+                    "name": "NXT Token for NEC2018",
+                    "public_key": "0x4Ac80f604dA7c7Dd39B26774a84fB99c28Fe41e0",
+                    "symbol": "NXT",
+                    "type": "asset"
+                  },
+                  ...
+                ]
+        """
         return self._request('GET', '/assets').get('data')
 
     def get_asset(self, asset_id):
+        """
+            Get assets by asset_id
+
+            response:
+                {
+                  "asset_id": "07065d64-fd33-39b5-b275-9a2cc4806ef4",
+                  "balance": "999607",
+                  "chain_id": "43d61dcd-e413-450d-80b8-101d5e903357",
+                  "confirmations": 100,
+                  "name": "NXT Token for NEC2018",
+                  "public_key": "0x4Ac80f604dA7c7Dd39B26774a84fB99c28Fe41e0",
+                  "symbol": "NXT",
+                  "type": "asset"
+                }
+        """
         return self._request('GET', '/assets/{}'.format(asset_id)).get('data')
 
-    def find_asset(self, symbol: str):
-        for x in self.list_assets():
-            if x['symbol'].upper() == symbol.upper():
-                return x
-
-    def asset_transactions(self, asset_id: str):
-        return self._request('GET', '/assets/{}/snapshots'.format(asset_id)).get('data')
-
     def transactions(self):
+        """
+            list transactions
+
+            response:
+                [
+                    {
+                      "amount": "0.1",
+                      "asset_id": "d0deee89-a0f3-34ec-a92c-1a4d16fd2c3d",
+                      "counter_user_id": "729c1eec-f03f-3a52-b391-6092cfaba3fb",
+                      "created_at": "2018-09-11T05:39:25.429083062Z",
+                      "memo": "",
+                      "opponent_id": "729c1eec-f03f-3a52-b391-6092cfaba1fb",
+                      "snapshot_id": "9833191e-f379-4a18-ae49-5199e1ce8a59",
+                      "trace_id": "49b990c7-cd3b-415b-8c5e-2f5909854202",
+                      "type": "transfer"
+                    },
+                    ...
+                ]
+        """
         return self._request('GET', '/snapshots').get('data')
 
     def user(self, user_id):
+        """
+            get user detail
+
+            response
+            {
+              "avatar_url": "",
+              "created_at": "2018-09-04T03:34:18.484856821Z",
+              "full_name": "Example",
+              "identity_number": "0",
+              "is_verified": false,
+              "mute_until": "0001-01-01T00:00:00Z",
+              "relationship": "STRANGER",
+              "type": "user",
+              "user_id": "729c1eec-f03f-3a52-b391-6092cfaba3fb"
+            }
+        """
         return self._request('GET', '/users/{}'.format(user_id)).get('data')
 
     pass
