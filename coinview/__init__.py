f18359fdcb30b226b3c4f25c8803e021affe06d5
_ <_@coinjinja.com>
2018-10-29 11:07:46 +0900

    fix docstring for `trasfer`

--- coinview/__init__.py
+++ coinview/__init__.py
@@ -115,20 +115,28 @@ class CoinViewPay:
         return r.json()
 
     def transfer(self, receiver_id: str, amount: str, asset_id: str, trace_id: str, memo: Optional[str]):
-        """
-            make a direct transfer
+        """Make a direct transfer
 
-            response
-                {
-                  "type": "transfer",
-                  "snapshot_id": "ab56be4c-5b20-41c6-a9c3-244f9a433f35",
-                  "opponent_id": "a465ffdb-4441-4cb9-8b45-00cf79dfbc46",
-                  "asset_id": "43d61dcd-e413-450d-80b8-101d5e903357",
-                  "amount": "-10",
-                  "trace_id": "7c67e8e8-b142-488b-80a3-61d4d29c90bf",
-                  "memo": "hello",
-                  "created_at": "2018-05-03T10:08:34.859542588Z"
-                }
+        :param receiver_id: recipient user id
+        :param amount: amount of token you with to transfer
+        :param asset_id: asset id of the token
+        :param trace_id: a UUID for identifying the transfer
+        :param memo: optional note
+
+        example response
+
+        .. code::
+
+            {
+              "type": "transfer",
+              "snapshot_id": "ab56be4c-5b20-41c6-a9c3-244f9a433f35",
+              "opponent_id": "a465ffdb-4441-4cb9-8b45-00cf79dfbc46",
+              "asset_id": "43d61dcd-e413-450d-80b8-101d5e903357",
+              "amount": "-10",
+              "trace_id": "7c67e8e8-b142-488b-80a3-61d4d29c90bf",
+              "memo": "hello",
+              "created_at": "2018-05-03T10:08:34.859542588Z"
+            }
         """
         return self._request('POST', '/transfers', {
             'asset_id': asset_id,
